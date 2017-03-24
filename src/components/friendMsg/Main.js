b9337565b14685ae28817a92031758d93c3e58e7
yeyuguo <346243440@qq.com>
2017-03-24 13:11:20 +0800

    聊天选项卡-换成 antd-mobile List Brief 主副标题

--- src/components/friendMsg/Main.js
+++ src/components/friendMsg/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {List} from 'antd-mobile'
 const Item = List.Item;
+const Brief = Item.Brief;
 
 require('antd-mobile/dist/antd-mobile.css')
 // 此处再次引入 antd-mobile.css 是为了解决 antd-mobile 会在最后加载的情况
@@ -25,7 +26,7 @@ const FriendMsg = React.createClass({
                 {
                     avator:'../../images/mn2.jpg',
                     userName:'玲玲',
-                    userID:'001',
+                    userID:'002',
                     msgInfo:[{
                         latestModify:'2017-03-16 13:30:25',
                         latestMsg:'你好，很高兴认识你！你好，很高兴认识你！你好，很高兴认识你！'
@@ -36,9 +37,9 @@ const FriendMsg = React.createClass({
                     isRead:false                
                 },
                 {
-                    avator:'../../images/mn3.jpg',
+                    avator:'',
                     userName:'玲玲',
-                    userID:'001',
+                    userID:'003',
                     msgInfo:[{
                         latestModify:'星期三',
                         latestMsg:'你好，很高兴认识你！'
@@ -58,30 +59,31 @@ const FriendMsg = React.createClass({
             <List renderHeader={() => ''} style={{'height':this.props.height}} className="friendMsg-list">
                 {this.state.users.map(function(item,index){
                     const data = item
-                    console.log({data})
+                    // console.log({data})
                     return (
-                        <Item onClick={this.chatWindow} key={index} style={{}}>
+                        <Item
+                        arrow="horizontal"
+                        thumb='../../images/mn1.jpg'
+                        multipleLine
+                        onClick={this.chatWindow} 
+                        key={index}
+                        extra={data.msgInfo[data.msgInfo.length-1].latestModify.split(' ').length>1?data.msgInfo[(data.msgInfo.length-1)].latestModify.split(' ')[1].substr(0,5):data.msgInfo[(data.msgInfo.length-1)].latestModify}
+                        >
+                            {data.userName}
+                            <Brief>{data.msgInfo[data.msgInfo.length-1].latestMsg}</Brief>
+                            {/*
                             <div className='friendMsg'>
                                 <img src={data.avator ? data.avator : this.state.defaultAvator} alt=""/>
                                 <div className="friendMsg-content">
+                                    
                                     <p>
                                         <span className="userName">{data.userName}</span>
-                                        <span className='latestTime'>{
-                                            data.msgInfo[data.msgInfo.length-1].latestModify.split(' ').length>1?data.msgInfo[(data.msgInfo.length-1)].latestModify.split(' ')[1].substr(0,5):data.msgInfo[(data.msgInfo.length-1)].latestModify
-                                            /*
-                                            data.msgInfo[msgIndex].latestModify.split(' ').length>1?data.msgInfo.latestModify.split(' ')[1].substr(0,5):data.msgInfo.latestModify
-                                            if(data.msgInfo.latestModify.split(' ').length>1){
-                                                return data.msgInfo.latestModify.split(' ')[1].substr(0,5)
-                                            }
-                                            else{
-                                                return data.msgInfo.latestModify
-                                            }
-                                            */
-                                        }</span>
+                                        <span className='latestTime'>{data.msgInfo[data.msgInfo.length-1].latestModify.split(' ').length>1?data.msgInfo[(data.msgInfo.length-1)].latestModify.split(' ')[1].substr(0,5):data.msgInfo[(data.msgInfo.length-1)].latestModify}</span>
                                     </p>
                                     <p className="latestMsg">{data.msgInfo[data.msgInfo.length-1].latestMsg}</p>
                                 </div>
                             </div>
+                            */}
                         </Item>
                         
                     )
